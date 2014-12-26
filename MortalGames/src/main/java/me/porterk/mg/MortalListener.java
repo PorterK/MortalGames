aa9b00c1a88367643e1ee5ef9a296b22435f22dd
Joannou1 <JohnnyAnimator@aol.com>
2014-12-26 06:52:43 -0800

    Making sure it still works after a bunch of jenkins restarts and stuff.

--- MortalGames/src/main/java/me/porterk/mg/MortalListener.java
+++ MortalGames/src/main/java/me/porterk/mg/MortalListener.java
@@ -39,7 +39,7 @@ public class MortalListener implements Listener{
 		
 		Main.getInstance().chat.put(p.getName(), "general");
 		
-		p.sendMessage(ChatColor.DARK_RED + "Testing the automated " + ChatColor.GOLD + "stuff 2.");
+		p.sendMessage(ChatColor.DARK_RED + "Testing the automated " + ChatColor.GOLD + "stuff 3.");
 		
 		if(playerCount < 10){
 			p.sendMessage(ChatColor.GOLD + "Welcome, "  + ChatColor.GREEN + p.getDisplayName() +
